22c898f2e975ee65631e24a546b746eb16457bc6
RiffRaff2009 <john@weatheritt.co.uk>
2023-03-17 14:50:19 +0000

    End of section 8

--- API/Data/Migrations/20230317103633_ExtendedUserEntity.Designer.cs
+++ API/Data/Migrations/20230317103633_ExtendedUserEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230317103633_ExtendedUserEntity")]
+    partial class ExtendedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.3");
@@ -36,9 +39,6 @@ namespace API.Data.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<DateOnly>("DateOfBirth")
-                        .HasColumnType("TEXT");
-
-                    b.Property<string>("Gender")
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Interests")
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using API.Extensions;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Entities
 {
@@ -11,5 +13,29 @@ namespace API.Entities
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string Gender {get; set;}
+        
+        public string KnownAs {get; set;}
+
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+
+        public string AboutMe { get; set; }
+
+        public string LookingFor {get; set;}
+
+        public string Interests  { get; set; }
+
+        public string City {get; set;}
+
+        public string Country { get; set; }
+
+        public List<Photo> Photos{get;set;} = new();
+
+    
     }
 }
