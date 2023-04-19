14b83e66b9c2c53e3dc2b082041488583c30a820
RiffRaff2009 <john@weatheritt.co.uk>
2023-04-19 17:40:22 +0100

    End of section 12 and rename solution

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -14,6 +14,12 @@ namespace API.DTOs
         [StringLength(8, MinimumLength =4)]
         public string Password { get; set; }
 
+        [Required] public string KnownAs { get; set; }
+
+        [Required] public string Gender { get; set; }
+        [Required] public DateOnly? DateOfBirth { get; set; }
+        [Required] public string City { get; set; }
+        [Required] public string Country { get; set; }
 
     }
 }
