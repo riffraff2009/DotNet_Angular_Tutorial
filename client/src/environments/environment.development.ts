83aa1634275e73583046283c8b554fc51f840124
RiffRaff2009 <john@weatheritt.co.uk>
2023-06-16 15:17:00 +0100

    Env

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:5001/api/',
-    hubUrl: 'https://localhost:5001/hubs/'
+    production: true,
+    apiUrl: 'api/',
+    hubUrl: 'hubs/'
 };
